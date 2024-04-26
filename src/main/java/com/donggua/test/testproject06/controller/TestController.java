65d5af31d1461b6d181d8106e999db630bfacb2a
dengwujun <992133520@qq.com>
2024-04-26 11:04:21 +0800

    整合 elasticsearch 收集idea log4j2日志

--- src/main/java/com/donggua/test/testproject06/controller/TestController.java
+++ src/main/java/com/donggua/test/testproject06/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.donggua.test.testproject06.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -38,6 +41,20 @@ public class TestController {
         return map;
     }
 
+    private Logger logger = LogManager.getLogger(TestController.class);
 
+    @RequestMapping(value = "/log4j2", method = RequestMethod.GET)
+    public String testLog(){
+        try {
+            logger.info("Hello 这是 info message. 信息");
+            logger.error("Hello 这是 error message. 报警");
+            logger.warn("Hello 这是 warn message. 警告");
+            logger.debug("Hello 这是 debug message. 调试");
+            logger.fatal("Hello 这是 fatal message. 严重");
+        } catch (Exception e) {
+            logger.error("testLog", e);
+        }
+        return "log success";
+    }
 
 }
